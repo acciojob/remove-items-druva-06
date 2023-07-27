9cef2ce2fc3e6626ce2c065213e29eca79399c5c
Druvasena Reddy <66788819+druva-06@users.noreply.github.com>
2023-07-27 21:26:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+const rmvClrBtn = document.getElementsByTagName('input')[0]
+
+rmvClrBtn.addEventListener('click',() => {
+    const colorSelect = document.getElementById('colorSelect')
+
+    const clr = colorSelect.value
+    
+    let clrElements = colorSelect.children
+
+    for(let i = 0; i < clrElements.length; i++){
+        if(clrElements[i].value == clr){
+            clrElements[i].remove()
+        }
+    }
+
+})
